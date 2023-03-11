50ceb0faee76dc5467dc6c59454e2b44188d3843
Maiakr <108643768+Maiakr@users.noreply.github.com>
2023-03-11 16:29:42 -0300

    actualizacion de PersonaController

--- src/main/java/com/portfolio/mk/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mk/Controller/PersonaController.java
@@ -1,7 +1,7 @@
 package com.portfolio.mk.Controller;
 
 import com.portfolio.mk.Entity.Persona;
-import com.portfolio.mk.Interface.IPersonaService;
+import com.portfolio.mk.Service.ImpPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +12,23 @@ import java.util.List;
 @CrossOrigin(origins = "https://frontendmk-a7321.web.app")
 
 public class PersonaController {
-    @Autowired IPersonaService ipersonaService;
+    @Autowired
+    ImpPersonaService personaService;
 
     @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
-        return ipersonaService.getPersona();
+        return personaService.getPersona();
     }
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
-        ipersonaService.savePersona(persona);
+        personaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
-        ipersonaService.deletePersona(id);
+        personaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     @PreAuthorize("hasRole('ADMIN')")
@@ -36,18 +37,18 @@ public class PersonaController {
                                 @RequestParam("nombre")String nuevoNombre,
                                 @RequestParam("apellido")String nuevoApellido,
                                 @RequestParam("img")String nuevoImg) {
-        Persona persona = ipersonaService.findPersona(id);
+        Persona persona = personaService.findPersona(id);
 
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
         persona.setImg(nuevoImg);
 
-        ipersonaService.savePersona(persona);
+        personaService.savePersona(persona);
         return persona;
 
     }
     @GetMapping("/personas/traer/perfil")
     public Persona findPersona() {
-        return ipersonaService.findPersona((long)1);
+        return personaService.findPersona((long)1);
     }
 }
